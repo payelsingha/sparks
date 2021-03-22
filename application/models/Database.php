6ec441e88056d12aaedb49a31c157f1635d3cf9e
payelsingha <jnvpayel@gmail.com>
2021-03-22 10:55:40 +0530

    first commit

---
+++ application/models/Database.php
@@ -0,0 +1,26 @@
+<?php
+class Database extends CI_Model{
+
+    public function show_datas()
+	{
+		$this->db->select('*');
+		$this->db->from('user_table');
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+    public function show_name()
+	{
+		$this->db->select('*');
+		$this->db->from('trans_user');
+		$query = $this->db->get();
+		return $query->result();
+	}
+    
+    public function user_registration($data)
+    { 
+        
+         $this->db->insert('trans_user',$data);
+        
+    }
+}
